226a930ae57eb5b7cdb32314aec24fe14cd919e8
kwonjs1996 <kwonjs1996@gmail.com>
2022-03-17 22:18:39 +0900

    useState 여러번 사용하기*

--- 01-starting-setup/src/components/NewExpenses/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpenses/ExpenseForm.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleCangeHandler = (e) => {
-    console.log(e.target.value);
+    setEnteredTitle(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
   };
 
   return (
@@ -15,11 +27,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="nember" min="0.01" step="0.01" />
+          <input
+            type="nember"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-ecpense__actions">
