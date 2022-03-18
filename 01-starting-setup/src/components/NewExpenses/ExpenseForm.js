e6560ba2b282365eb0a94507a2d47491ac19d8e7
kwonjs1996 <kwonjs1996@gmail.com>
2022-03-18 12:43:10 +0900

    Form에서의 양방향 바인딩 중요!

--- 01-starting-setup/src/components/NewExpenses/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpenses/ExpenseForm.js
@@ -61,7 +61,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleCangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleCangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -69,6 +73,7 @@ const ExpenseForm = () => {
             type="nember"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -78,6 +83,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
