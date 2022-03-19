2e31c7f6e9a77e968358fe760dfd4ce6128524b8
kwonjs1996 <kwonjs1996@gmail.com>
2022-03-19 14:57:14 +0900

    자식 - 부모 컴포넌트 통신 props

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -24,6 +24,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   /*   return React.createElement(
     "div",
     {},
@@ -33,7 +38,7 @@ function App() {
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses item={expenses} />
     </div>
   );
