83e808b5de22f84aaa8fc48e25ea867370d91255
kwonjs1996 <kwonjs1996@gmail.com>
2022-03-27 18:42:45 +0900

    작은 에러 수정

--- 01-starting-setup/src/components/NewExpenses/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpenses/ExpenseForm.js
@@ -49,7 +49,7 @@ const ExpenseForm = (props) => {
     e.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
